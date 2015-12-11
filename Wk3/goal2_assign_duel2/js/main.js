9983568343eefe35b5012f39c773d020178fed29
astrawter <afstrawter@gmail.com>
2015-12-11 06:08:02 -0600

    removed parts of for loop
    changed elements to to DOM and got the scores working.

--- Wk3/goal2_assign_duel2/js/main.js
+++ Wk3/goal2_assign_duel2/js/main.js
@@ -10,35 +10,47 @@ Assignment Goal 2: Duel 2
 /*Create three variables for each player of the two players. Player’s name, health, and damage*/
 
 //Spiderman's name ,health, and damage
-var playerOne = ["SPIDERMAN", 100, 20];
 
-
+var playerOne = {name:"SPIDERMAN", health:100, damage:20};
 
 //Batman's name ,health, and damage
-var playerTwo = ["BATMAN", 100, 20];
+
+var playerTwo = {name:"BATMAN", health:100, damage:20};
+
+//Both fighter objects should be in an array
+
+var fighters = [playerOne, playerTwo];
+
     
 //Rounds
 var round = 0;
 
 
+//Replace the alerts with code that access the DOM(HTML) such as getElemetById and/or querySelector
 /*fight function loops through each round and reduces players health based on a randomly generated number*/
+
 function fight(){
-    alert(playerOne[0] + ":" + playerOne[1] + " *START* " + playerTwo[0] + ":" +     playerTwo[1]); 
-    
+    alert(playerOne.name + ":" + playerOne.health + " *START* " + playerTwo.name + ":" +     playerTwo.health);
+
         //loops the function until 10 rounds
-    for (var i = 0; i < 10; i++){ 
+
+     var start = docuument.getElementByClass('buttonblue');
+    console.log(start);
+    start.addEventListener('click', calc)
+
+    var calc = function(){
         
         //formula to get a min damage for player 1 and player 2
-        var minDamage1 = playerOne[2] * .5;
-        var minDamage2 = playerTwo[2] * .5;
+        var minDamage1 = playerOne.damage * .5;
+        var minDamage2 = playerTwo.damage * .5;
         
         //formula to get a random number damage for player 1 and player 2
-        var f1 = Math.floor(Math.random()*(playerOne[2]-minDamage1) + minDamage1);
-        var f2 = Math.floor(Math.random()*(playerTwo[2]-minDamage2) + minDamage2);
+        var f1 = Math.floor(Math.random()*(playerOne.damage-minDamage1) + minDamage1);
+        var f2 = Math.floor(Math.random()*(playerTwo.damage-minDamage2) + minDamage2);
         
         //subtract random damage from players health
-        playerOne[1] -= f1;
-        playerTwo[1] -= f2;
+        playerOne.health -= f1;
+        playerTwo.health -= f2;
         
     //pulls results from the winnerCheck function
    var results = winnerCheck();
@@ -49,17 +61,23 @@ function fight(){
        round++;
        
        //alerts player the score after each round
-       alert(playerOne[0] + ":" + playerOne[1] + " *ROUND " + round + " OVER* " +  playerTwo[0] + ":" +     playerTwo[1]);  
+       //capture location of score bar
+       var bar = document.querySelectorAll('#scores div')
+       console.log(bar);
+
+//Shows change in health Fighter's name and health at the top
+       bar[0].innerHTML = 'Spiderman: ' + playerOne.health ;
+       bar[1].innerHTML = playerTwo.health ;
+       alert(playerOne.name + ":" + playerOne.health + " *ROUND " + round + " OVER* " +  playerTwo.name + ":" +     playerTwo.health);
    }else{
        
        //if there is a winner or tie displays results
        alert(results);
        
-       //breaks out of the FOR loop to declare winner
-       break;  
      };
    };
 };
+
     
 //Checks for a winner after every round 
 function winnerCheck(){
@@ -68,14 +86,14 @@ function winnerCheck(){
     var result = "no winner";
     
     //if both players health becomes 0 or less there is a TIE
-    if (playerOne[1] < 1 && playerTwo[1] <1 ){
+    if (playerOne.health < 1 && playerTwo.health <1 ){
         result = "You Both Die";
         
     //if one player's health reaches 0 or less they are the  LOSER  
-    }else if(playerOne[1]<1){
-        result = playerTwo[0] + " WINS!!!"
-    }else if(playerTwo[1]<1){
-        result = playerOne[0] + " WINS!!!"
+    }else if(playerOne.health<1){
+        result = playerTwo.name + " WINS!!!"
+    }else if(playerTwo.health<1){
+        result = playerOne.name + " WINS!!!"
     };
     
     //returns results to fight function
@@ -87,3 +105,4 @@ fight();
     
 //end self executing function 
 })();
+
