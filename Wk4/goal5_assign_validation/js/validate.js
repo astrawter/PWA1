ce2a5f516df178e3d8f8039d987a02a3f6004555
astrawter <afstrawter@gmail.com>
2015-12-17 14:19:17 -0600

    started adding elements to code

--- Wk4/goal5_assign_validation/js/validate.js
+++ Wk4/goal5_assign_validation/js/validate.js
@@ -1,16 +1,31 @@
 /*
-     Developed by the JavaScript team at Fullsail
-     Date: 1306
+ username (id = f_username)
+ email (id = f_email)
+ phone number (id = f_phone)
+ social security number (id = f_ssn)
+ password (id = f_password)
 */
 
 (function(){
+
+
 
     myform.onsubmit = function(e){
 
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
+        var myform = document.getElementById('myform');
+        var username = document.getElementById('f_username');
+        var email = document.getElementById('f_email');
+        var phoneNum = document.getElementById('f_phone');
+        var ssn = document.getElementById('f_ssn');
+        var pw = document.getElementById('f_password');
 
-        validateField(id);  //id = is the form input field ID
+        validateField(username);  //id = is the form input field ID
+        validateField(email);
+        validateField(phoneNum);
+        validateField(ssn);
+        validateField(pw);
 
 
         e.preventDefault();
@@ -19,12 +34,20 @@
 
 
     var validateField = function(inputName){
-
-        if (inputName.name === 'id name goes here'){
-            var pattern = 'a RegEx pattern goes here'
-
-            //You will need to create an else-if statement for each input field id.  The
-            //      format will be similar to the above IF statement.
+        if (inputName.f_username === 'username'){
+            var pattern = /^[A-Z]\w*\s[A-Z]\w*/;
+        }
+        else if (inputName.f_email === 'email'){
+            var pattern2 = /^\w+(@)[a-zA-Z_]+?\.[a-zA-Z\.]{1,4}$/;
+        }
+        else if (inputName.f_phone === 'phoneNum'){
+            var pattern3 = /^([\(]{1}[0-9]{3}[\)]{1}[0-9]{3}[\-]{1}[0-9]{4})$/;
+        }
+        else if (inputName.f_ssn === 'ssn'){
+            var pattern4 = /^\d{3}-\d{2}-\d{4}$/;
+        }
+        else if (inputName.f_password === 'pw'){
+            var pattern5 = /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/;
 
 
         };
